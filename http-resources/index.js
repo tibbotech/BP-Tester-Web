00f82df011bf09a2c702a177b2e7646de6bf451c
Hsiang307 <hsiang@tibbo.com>
2018-11-19 15:01:51 +0800

    Added monitor upgrade functionality

--- http-resources/index.js
+++ http-resources/index.js
@@ -10,6 +10,7 @@ const AppRouter = () => (
       <Route path="/setid/" component={SetID} />
       <Route path="/scan/" component={Scan} />
       <Route path="/monitor/" component={Monitor} />
+      <Route path="/uploadmonitor/" component={UploadMonitor} />
     </div>
   </Router>
 );
@@ -23,21 +24,27 @@ class MainMenu extends React.Component {
       <div>
         <br></br><h1>Hexagon Sensor Test</h1><br></br><br></br>
         <div class="row" align="center">
-          <div class="col-xs-12 col-md-4">
+          <div class="col-xs-12 col-md-6">
             <Link to="/setid/" style={{ textDecoration: 'none' }}><button class="button">
               Set ID
               </button></Link>
           </div>
-          <div class="col-xs-12 col-md-4">
+          <div class="col-xs-12 col-md-6">
             <Link to="/scan/" style={{ textDecoration: 'none' }}> <button class="button">
               Scan
               </button></Link>
           </div>
-          <div class="col-xs-12 col-md-4">
+          <div class="col-xs-12 col-md-6">
             <Link to="/monitor/" style={{ textDecoration: 'none' }}><button class="button">
               Monitor Sensors
               </button></Link>
           </div>
+          <div class="col-xs-12 col-md-6">
+            <Link to="/uploadmonitor/" style={{ textDecoration: 'none' }}><button class="button">
+              Upload Monitor
+              </button></Link>
+          </div>
+          
         </div>
       </div>
     );
@@ -72,6 +79,95 @@ class SetID extends React.Component {
     value = document.getElementById("address").value;
     jQuery.ajax("sendsetidcommand.html?value=" + value);
     alert("Set ID command sent");
+  }
+}
+
+class UploadMonitor extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { uploadScreen: 1, uploadSuccessful: false }
+  }
+
+  startUpload() {
+    var percent = 0
+    this.interval = setInterval(() => {
+      percent++;
+      document.getElementById("percent").innerHTML = (percent);
+      document.getElementById("progbar").style.width = (percent) + "%";
+      if (percent >= 100) {
+        percent = 0
+        clearInterval(this.interval);
+        { this.uploadComplete() }
+      }
+    }, 120);
+  }
+
+  sendUploadMonitorCommand() {
+    jQuery.ajax("senduploadmonitorcommand.html");
+    this.setState({ uploadScreen: 2 });
+  }
+
+  uploadComplete() {
+    document.getElementById("title").innerHTML = 'Upload Complete';
+    $.getJSON("json.html", function (data) {
+      this.setState({ uploadSuccessful: data.uploadsuccessful,  uploadScreen: 3 });
+    }.bind(this));
+  }
+
+  render() {
+    if (this.state.uploadScreen === 1) {
+      return (
+        <div>
+          <br></br><h1 class="center">Upload Monitor</h1><br></br>
+          <h2 class="center">
+            Disconnect sensor power<br></br>
+            Press and hold MD button on sensor<br></br>
+            Connect power without releasing MD button<br></br>
+            Release the MD button<br></br>
+            Press upload<br></br><br></br>
+            <button class="smallbutton" onClick={this.sendUploadMonitorCommand.bind(this)}>Upload</button><br></br>
+            <Link to="/"><button class="smallbutton">Main Menu</button></Link>
+          </h2>
+        </div>
+      );
+    } else if (this.state.uploadScreen === 2) {
+      return (
+        <div>
+          {this.startUpload()}
+          <br></br><h1><span id="title">Uploading Monitor...</span></h1><br></br>
+          <h2>
+            <div id="body" align='center'>
+              <div id="percentupdate"><b><span id="percent"></span>% Complete</b></div><br></br>
+              <div class="progress" id="progressb">
+                <div id="progbar" class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+          </h2>
+        </div>
+      )
+    } else if (this.state.uploadSuccessful === 1) {
+      return (
+        <div>
+          <div>
+            <h1>Upload Successful</h1> <br></br> <br></br>
+            <h2 class="center">Please reboot sensor for installation to complete<br></br><br></br><br></br></h2>
+          </div>
+          <h2><Link to="/"><button class="smallbutton">Main Menu</button></Link></h2>
+        </div>
+      )
+    } else if (this.state.uploadSuccessful === 0) {
+      return (
+        <div>
+          <div>
+            <h1>Upload Failed!</h1> <br></br> <br></br>
+            <h2 class="center">Please try again!<br></br><br></br><br></br></h2>
+          </div>
+          <h2><Link to="/"><button class="smallbutton">Main Menu</button></Link></h2>
+        </div>
+      )
+    } 
   }
 }
 
@@ -206,7 +302,7 @@ class Monitor extends React.Component {
         </table>
           <br></br><br></br>
           <Link to="/"><button class="smallbutton">Main Menu</button></Link><br></br><br></br></h2>
-          
+
       )
     }
   }
