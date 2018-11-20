419a46ab02fe17969bf0fed92c9bc90a67af0bc6
Hsiang307 <hsiang@tibbo.com>
2018-11-20 09:14:27 +0800

    Added firmware and monitor version display

--- http-resources/index.js
+++ http-resources/index.js
@@ -11,6 +11,10 @@ const AppRouter = () => (
       <Route path="/scan/" component={Scan} />
       <Route path="/monitor/" component={Monitor} />
       <Route path="/uploadmonitor/" component={UploadMonitor} />
+      <Route path="/uploadfirmware/" component={UploadFirmware} />
+      <Route path="/setupmenu/" component={SetUp} />
+      <Route path="/monitormenu/" component={MonitorMenu} />
+      <Route path="/viewfirmware/" component={ViewFirmware} />
     </div>
   </Router>
 );
@@ -25,26 +29,84 @@ class MainMenu extends React.Component {
         <br></br><h1>Hexagon Sensor Test</h1><br></br><br></br>
         <div class="row" align="center">
           <div class="col-xs-12 col-md-6">
-            <Link to="/setid/" style={{ textDecoration: 'none' }}><button class="button">
-              Set ID
+            <Link to="/setupmenu/" style={{ textDecoration: 'none' }}><button class="button">
+              Set Up Sensors
               </button></Link>
           </div>
           <div class="col-xs-12 col-md-6">
-            <Link to="/scan/" style={{ textDecoration: 'none' }}> <button class="button">
-              Scan
-              </button></Link>
-          </div>
-          <div class="col-xs-12 col-md-6">
-            <Link to="/monitor/" style={{ textDecoration: 'none' }}><button class="button">
+            <Link to="/monitormenu/" style={{ textDecoration: 'none' }}> <button class="button">
               Monitor Sensors
               </button></Link>
           </div>
-          <div class="col-xs-12 col-md-6">
+        </div>
+      </div>
+    );
+  }
+}
+
+class SetUp extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <br></br><h1>Set Up Sensors</h1><br></br><br></br>
+        <div class="row" align="center">
+          <div class="col-xs-12 col-md-4">
             <Link to="/uploadmonitor/" style={{ textDecoration: 'none' }}><button class="button">
               Upload Monitor
+                  </button></Link>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <Link to="/uploadfirmware/" style={{ textDecoration: 'none' }}><button class="button">
+              Upload Firmware
+                  </button></Link>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <Link to="/setid/" style={{ textDecoration: 'none' }}><button class="button">
+              Set ID
+                  </button></Link>
+          </div>
+
+        </div>
+        <div class="center">
+        <br></br><br></br><Link to="/"><button class="smallbutton">Main Menu</button></Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+class MonitorMenu extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <br></br><h1>Monitor Sensors</h1><br></br><br></br>
+        <div class="row" align="center">
+          <div class="col-xs-12 col-md-4">
+            <Link to="/scan/" style={{ textDecoration: 'none' }}><button class="button">
+              Scan
               </button></Link>
           </div>
-          
+          <div class="col-xs-12 col-md-4">
+            <Link to="/viewfirmware/" style={{ textDecoration: 'none' }}><button class="button">
+              View Firmware Versions
+              </button></Link>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <Link to="/monitor/" style={{ textDecoration: 'none' }}> <button class="button">
+              Monitor Sensors
+              </button></Link>
+          </div>
+        </div>
+        <div class="center">
+        <br></br><br></br><Link to="/"><button class="smallbutton">Main Menu</button></Link>
         </div>
       </div>
     );
@@ -100,7 +162,7 @@ class UploadMonitor extends React.Component {
         clearInterval(this.interval);
         { this.uploadComplete() }
       }
-    }, 120);
+    }, 90);
   }
 
   sendUploadMonitorCommand() {
@@ -111,7 +173,7 @@ class UploadMonitor extends React.Component {
   uploadComplete() {
     document.getElementById("title").innerHTML = 'Upload Complete';
     $.getJSON("json.html", function (data) {
-      this.setState({ uploadSuccessful: data.uploadsuccessful,  uploadScreen: 3 });
+      this.setState({ uploadSuccessful: data.uploadsuccessful, uploadScreen: 3 });
     }.bind(this));
   }
 
@@ -167,9 +229,99 @@ class UploadMonitor extends React.Component {
           <h2><Link to="/"><button class="smallbutton">Main Menu</button></Link></h2>
         </div>
       )
-    } 
+    }
   }
 }
+
+class UploadFirmware extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { uploadScreen: 1, uploadSuccessful: false }
+  }
+
+  startUpload() {
+    var percent = 0
+    this.interval = setInterval(() => {
+      percent++;
+      document.getElementById("percent").innerHTML = (percent);
+      document.getElementById("progbar").style.width = (percent) + "%";
+      if (percent >= 100) {
+        percent = 0
+        clearInterval(this.interval);
+        { this.uploadComplete() }
+      }
+    }, 90);
+  }
+
+  sendUploadMonitorCommand() {
+    jQuery.ajax("senduploadfirmwarecommand.html");
+    this.setState({ uploadScreen: 2 });
+  }
+
+  uploadComplete() {
+    document.getElementById("title").innerHTML = 'Upload Complete';
+    $.getJSON("json.html", function (data) {
+      this.setState({ uploadSuccessful: data.uploadsuccessful, uploadScreen: 3 });
+    }.bind(this));
+  }
+
+  render() {
+    if (this.state.uploadScreen === 1) {
+      return (
+        <div>
+          <br></br><h1 class="center">Upload Firmware</h1><br></br>
+          <h2 class="center">
+            Disconnect sensor power<br></br>
+            Press and hold MD button on sensor<br></br>
+            Connect power without releasing MD button<br></br>
+            Release the MD button<br></br>
+            Press upload<br></br><br></br>
+            <button class="smallbutton" onClick={this.sendUploadMonitorCommand.bind(this)}>Upload</button><br></br>
+            <Link to="/"><button class="smallbutton">Main Menu</button></Link>
+          </h2>
+        </div>
+      );
+    } else if (this.state.uploadScreen === 2) {
+      return (
+        <div>
+          {this.startUpload()}
+          <br></br><h1><span id="title">Uploading Firmware...</span></h1><br></br>
+          <h2>
+            <div id="body" align='center'>
+              <div id="percentupdate"><b><span id="percent"></span>% Complete</b></div><br></br>
+              <div class="progress" id="progressb">
+                <div id="progbar" class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+                </div>
+              </div>
+            </div>
+          </h2>
+        </div>
+      )
+    } else if (this.state.uploadSuccessful === 1) {
+      return (
+        <div>
+          <div>
+            <h1>Upload Successful</h1> <br></br> <br></br>
+            <h2 class="center">Please reboot sensor for installation to complete<br></br><br></br><br></br></h2>
+          </div>
+          <h2><Link to="/"><button class="smallbutton">Main Menu</button></Link></h2>
+        </div>
+      )
+    } else if (this.state.uploadSuccessful === 0) {
+      return (
+        <div>
+          <div>
+            <h1>Upload Failed!</h1> <br></br> <br></br>
+            <h2 class="center">Please try again!<br></br><br></br><br></br></h2>
+          </div>
+          <h2><Link to="/"><button class="smallbutton">Main Menu</button></Link></h2>
+        </div>
+      )
+    }
+  }
+}
+
 
 class Scan extends React.Component {
   constructor() {
@@ -189,7 +341,7 @@ class Scan extends React.Component {
         clearInterval(this.interval);
         { this.scanComplete() }
       }
-    }, 100);
+    }, 400);
   }
 
   scanComplete() {
@@ -257,7 +409,7 @@ class Monitor extends React.Component {
           sensors: data.sensors
         })
       })
-    }, 200);
+    }, 400);
   }
 
   createHeaders = () => {
@@ -275,6 +427,80 @@ class Monitor extends React.Component {
           <td>{item.id}</td>
           <td>{item.type}</td>
           <td>{item.data}</td>
+        </tr>
+      )
+    })
+  }
+
+  render() {
+    if (this.state.sensors === null) {
+      return (
+        <div>
+        </div>
+      )
+    }
+    else if (this.state.sensors === 0) {
+      return (
+        <div><br></br>
+          <h2 class="center"><red>No sensors detected! Please ensure that sensors are connected correctly and have been assigned a UNIQUE ID, then scan again.</red><br></br><br></br>
+            <Link to="/"><button class="smallbutton">Main Menu</button></Link><br></br><br></br></h2>
+        </div>
+      )
+    } else if (this.state.sensors > 0) {
+      return (
+        <h2><br></br><table align='center'>
+          {this.createHeaders()}
+          {this.renderDataList()}
+        </table>
+          <br></br><br></br>
+          <Link to="/"><button class="smallbutton">Main Menu</button></Link><br></br><br></br></h2>
+
+      )
+    }
+  }
+}
+
+class ViewFirmware extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { dataList: [], sensors: null }
+    var sensorTypes = ["Unknown", "Temperature & Humidity", "Temperature", "Light", "", "Accelerometer"]
+    this.interval = setInterval(() => {
+      $.getJSON("json.html", (data) => {
+        var dataList = [];
+        for (let i = 0; i < data.id.length; i++) {
+          dataList.push({
+            id: data.id[i],
+            type: sensorTypes[data.type[i]],
+            monitor: data.monitor[i],
+            firmware: data.firmware[i],
+          })
+        }
+        this.setState({
+          dataList: dataList,
+          sensors: data.sensors
+        })
+      })
+    }, 400);
+  }
+
+  createHeaders = () => {
+    var names = ['ID', 'Type', 'Monitor', 'Firmware'];
+    var namesList = names.map(function (name) {
+      return <th>{name}</th>;
+    })
+    return <tr>{namesList}</tr>
+  }
+
+  renderDataList = () => {
+    return this.state.dataList.map(function (item) {
+      return (
+        <tr>
+          <td>{item.id}</td>
+          <td>{item.type}</td>
+          <td>{item.monitor}</td>
+          <td>{item.firmware}</td>
         </tr>
       )
     })
